c6d8ec88a9e38033889850495d257cc97bbececa
Minoru Natsutani <YRW03704@nifty.com>
2023-04-02 01:20:53 +0900

    単純なアサインサポート

--- honocc/genx64.fs
+++ honocc/genx64.fs
@@ -24,15 +24,38 @@ let pop(fp : StreamWriter, reg) =
 let number(fp : StreamWriter, num:NdNum) =
     fp.WriteLine $"  mov $%d{num.Value}, %%rax "
     
+// 変数テーブルからアドレスを計算して raxに入れる
+let gen_addr(fp : StreamWriter, ast, fn : Honoenv.Function) =
+    match ast with
+    | Variable(node_var) ->
+        let offset = fn.getOffset(node_var.Name)
+        fp.WriteLine $"  lea %d{offset}(%%rbp), %%rax" 
+    | _ -> failwith $"unsupported node  %A{ast}"
 
-let rec gen_expr(fp, ast) =
+let rec gen_expr(fp, ast, fn) =
     match ast with
     | Num(node_num) -> number(fp, node_num)
-    | BinOp(binop) ->
-        // Binopの左オペランドをrdi,右オペランドをraxに入れる。
-        gen_expr(fp, binop.r)
+    | BinOp(binop) -> gen_binop(fp, binop, fn)
+    | Variable(v) ->
+        gen_addr(fp, ast, fn)
+        fp.WriteLine "  mov (%rax), %rax"
+    | _ -> failwith $"unsupported node  %A{ast}"
+and gen_binop(fp, binop, fn) =
+    if binop.op = BinOpKind.Assign then
+        // rdiには左辺のアドレスが入っている。
+        // raxには右辺の値が入っている。
+        gen_addr(fp, binop.l, fn)
         push(fp)
-        gen_expr(fp, binop.l)
+        gen_expr(fp, binop.r, fn)
+        pop(fp, "%rdi")
+
+        fp.WriteLine "  mov %rax, (%rdi)"
+        fp.WriteLine "  mov %rax, %rdi"
+    else            
+        // Binopの左オペランドをrdi,右オペランドをraxに入れる。
+        gen_expr(fp, binop.r, fn)
+        push(fp)
+        gen_expr(fp, binop.l, fn)
         pop(fp, "%rdi")
         match binop.op with
             | BinOpKind.Add -> fp.WriteLine "  add %rdi, %rax"
@@ -95,12 +118,11 @@ let rec gen_expr(fp, ast) =
                  fp.WriteLine "  cmp $0, %rax"
                  fp.WriteLine "  setne %al"
                  fp.WriteLine "  movzb %al, %rax"
-            | _ -> failwith $"unsupported binop  %A{ast}"
-    | _ -> failwith $"unsupported node  %A{ast}"
     
-let gen_funccall(fp, funccall: NdFuncCall) =
+
+let gen_funccall(fp, funccall: NdFuncCall, fn) =
     // パラメータは１つのみ
-    gen_expr(fp, funccall.Params.Head)
+    gen_expr(fp, funccall.Params.Head, fn)
     
     //gen_exprの値はraxに入っている。
     //パラメータ一つの時はrdiに値を入れる。    
@@ -114,15 +136,19 @@ let gen_funccall(fp, funccall: NdFuncCall) =
     fp.WriteLine "  mov $0, %rax"
 
 
-let gen_body(fp, asts: Ast list) =
+let gen_body(fp, asts: Ast list, fn) =
     for ast in asts do
         match ast with
-        | Ast.FuncCall(funcall) -> gen_funccall(fp, funcall)
+        | Ast.FuncCall(funcall) -> gen_funccall(fp, funcall, fn)
+        | Ast.BinOp(_) -> gen_expr(fp, ast, fn)
         | _ -> failwith $"unsupported node  %A{ast}"
     
 
 let generate(asm_file_path : string,  fn : Honoenv.Function)=
      let fp = new StreamWriter(asm_file_path)
+     // Local variableのオフセット割当
+     fn.setLocalVariableOffset()
+     fn.printLocals()
      match fn.NdFunction with
      | Some(func) ->
          fp.WriteLine $".globl {func.Name}"
@@ -132,10 +158,10 @@ let generate(asm_file_path : string,  fn : Honoenv.Function)=
          fp.WriteLine "# Prologue"    
          fp.WriteLine "  push %rbp"        //ベースポインタを保存
          fp.WriteLine "  mov %rsp, %rbp"     //ベースポインタに関数に入った時のスタックポインタを保存
-         fp.WriteLine $"  sub $%d{0}, %%rsp"     //変数の領域確保
+         fp.WriteLine $"  sub $%d{fn.stackSize}, %%rsp"     //変数の領域確保
          fp.WriteLine ""  
          
-         gen_body(fp, func.Body)  |> ignore
+         gen_body(fp, func.Body, fn)  |> ignore
          assert (stack_count = 0)
          
          fp.WriteLine "# Epilogue"
--- honocc/honoenv.fs
+++ honocc/honoenv.fs
@@ -5,11 +5,16 @@ open Honodef
 type Function(name:string) =
     let mutable locals : Variable list = []
     let mutable nodeFunction : NdFunction option = None
+    
+    //stack size
+    let mutable total_stack_size = 0
     member prt.Name = name
     member prt.NdFunction
         with get() = nodeFunction
         and set(n) = nodeFunction <- n
         
+    member ptr.stackSize = total_stack_size
+    
     member prt.hasVariable(name:string) =
         let sameName x =
             x.Name = name
@@ -20,4 +25,28 @@ type Function(name:string) =
     member prt.addVariable(v:Variable) =
         locals <- locals @ [v]
     
+    member prt.setLocalVariableOffset() =
+        let mutable offset = 0
+        let mutable locals_tmp = []
+        let mutable stack_size = 0
+        for v in locals do
+            offset <- offset - v.Size
+            stack_size <- stack_size + v.Size
+            let new_v :Variable = {Name=v.Name ; Type=v.Type; Size =v.Size  ; Local = v.Local ; Offset = offset}
+            locals_tmp <- locals_tmp @ [new_v]
+        locals <- locals_tmp
+        total_stack_size <- stack_size
+        
+    member prt.printLocals() =
+        printfn $"%s{name}"
+        for v in locals do
+            printfn $"%s{v.Name} %A{v.Type}, offset = %d{v.Offset}"
+    
+    //変数名からoffsetの値を返す。なければ例外を投げる
+    member prt.getOffset(name:string) =
+        let sameName x =
+            x.Name = name
+        match List.tryFind sameName locals with
+        | Some v -> v.Offset
+        | None -> failwith $"%s{name} is not found"
     
